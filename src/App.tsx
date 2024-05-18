4ed60a5c4275f68bf030daf9901008698fdda314
Tereza Smolova <tereza.smolova@gmail.com>
2024-05-18 11:51:19 +0200

    Style

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Stage from './components/Stage/Stage';
 const App: React.FC = () => {
   return (
     <div className="container">
-      <h1>Datlování</h1>
+      <h1 className='h1'>Datlování</h1>
       <Stage />
     </div>
   );
